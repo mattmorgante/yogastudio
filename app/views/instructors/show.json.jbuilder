bed10441bed876c85f8c10c9064653ad9bab0cab
Matt <matthewmorgante@gmail.com>
2015-06-01 14:14:37 -0500

    instructors'

---
+++ app/views/instructors/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @instructor, :id, :name, :classname, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :instructors
   resources :classnames
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root to: "users#index"
+  root to: "classnames#index"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
